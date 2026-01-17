a268367a6775d6c7d1e884498bc55c1e15d40191
kStor2poche <43518872+kStor2poche@users.noreply.github.com>
2026-01-17 12:05:12 +0100

    some tweaks

--- dot_config/nvim/lua/config/styling.lua
+++ dot_config/nvim/lua/config/styling.lua
@@ -2,7 +2,7 @@ local m = {}
 
 m.setup = function()
     vim.api.nvim_set_hl(0, "FloatTitle", { link = "NormalFloat" })
-    -- vim.api.nvim_set_hl(0, "WinBarNC", { link = "WinBar" }) -- doesn't work for oil :(
+    vim.api.nvim_set_hl(0, "WinBarNC", { link = "WinBar", force = true })
     -- Try to override window border styling globally ?
     -- now can be done using vim.o.winborder
 end
